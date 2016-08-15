75de9bd6156eb9023ba37b76acccfe79db0fff93
Dave Swart <swart.dave@gmail.com>
2016-08-15 20:15:10 +0200

    Add addFamily.php

---
+++ admin/addFamily.php
@@ -0,0 +1,29 @@
+<?php
+require_once "config.php";
+$familyName = mysqli_real_escape_string($db, $_POST['familyName']);
+$inviteCode = mysqli_real_escape_string($db, $_POST['inviteCode']);
+echo $familyName;
+echo "<br>";
+echo $inviteCode;
+// $sql = "
+//    INSERT INTO family
+//    (
+//       familyName,
+//       familyInviteCode
+//    )
+//    VALUES
+//    (
+//       '".$familyName."',
+//       '".$inviteCode."'
+//    )
+// ";
+//
+// if(!$res = mysql_query($sql))
+// {
+//     trigger_error(mysql_error().'<br />In query: '.$sql);
+// }
+// else
+// {
+//     $id = mysql_insert_id();
+// }
+?>
