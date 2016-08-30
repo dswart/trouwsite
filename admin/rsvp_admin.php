cc27f93379ee1161c9054f68519bc5848647b509
Dave Swart <swart.dave@gmail.com>
2016-08-30 19:23:30 +0200

    Hope makes for belief

--- admin/rsvp_admin.php
+++ admin/rsvp_admin.php
@@ -45,9 +45,9 @@
             <label for="family">Hoort bij deze familie</label>
             <?php
                require_once "config.php";
-               $query = "SELECT familyName FROM family";
+               $query = "SELECT * FROM family";
                if(!mysqli_select_db($link, $db_name)){
-                  die('Error selecting databse: ' . mysqli_error($link));
+                  die('Error selecting database: ' . mysqli_error($link));
                }
                if(!mysqli_query($link, $query)){
                   die('Error inserting SQL: ' . mysqli_error($link));
