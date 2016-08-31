f0ca307c1165e3925e551dbb3916fc082ffa7471
Dave Swart <swart.dave@gmail.com>
2016-08-31 07:25:49 +0200

    Hope makes for belief

--- admin/addPerson.php
+++ admin/addPerson.php
@@ -11,4 +11,27 @@ $personName = mysqli_real_escape_string($link, $_POST['personName']);
 $family = mysqli_real_escape_string($link, $_POST['family']);
 echo $personName;
 echo $family;
+
+if(!mysqli_select_db($link, $db_name)){
+   die('Error selecting databse: ' . mysqli_error($link));
+}
+if(!mysqli_query($link, $sql)){
+   die('Error inserting SQL: ' . mysqli_error($link));
+}
+else{
+   echo 'Great success!';
+}
+
+if(!mysqli_select_db($link, $db_name)){
+   die('Error selecting databse: ' . mysqli_error($link));
+}
+if(!mysqli_query($link, $sql)){
+   die('Error inserting SQL: ' . mysqli_error($link));
+}
+else{
+   echo 'Great success!';
+}
+
 ?>
+</body>
+</html>
